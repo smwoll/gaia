b7477bcfe120ce82f31f36cb72842b1c9d83f984
Sky <skylandwoll@gmail.com>
2021-01-24 11:55:06 -0500

    AOS config once

--- src/js/components/aosImport.js
+++ src/js/components/aosImport.js
@@ -1,6 +1,7 @@
 // import AOS
 import AOS from 'aos';
 
+// initialize AOS and add any config if needed. I've set AOS to animate elements only once, when scrolling down.
 AOS.init({
   once: true,
 });
